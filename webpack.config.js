8fd60de19a1a6b6fc146a59b7705ad8b2040df8d
RossPin <dorpetz@hotmail.com>
2020-01-24 12:32:04 +1300

    add babel webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// webpack v4
+const path = require('path')
+
+module.exports = {
+  entry: { main: './client/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map'
+}
