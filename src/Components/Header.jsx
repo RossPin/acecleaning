32ae577f788ef4722afbe2f567191dda7c09c464
RossPin <dorpetz@hotmail.com>
2020-01-24 16:31:18 +1300

    non capitalised class names

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-      <div className="Header">
+      <div className="header">
         <h1>Ace Cleaning</h1>        
       </div>
     );
